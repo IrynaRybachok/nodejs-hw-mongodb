71676afdfaadee88f033f0cf4acb3b781cecba77
Iryna R <deutsche.post15@gmail.com>
2024-11-18 22:15:04 +0100

    init and do task 1, 2

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import cors from 'cors';
+import express from 'express';
+import pino from 'pino-http';
+
+export function setupServer() {
+  const app = express();
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use((req, res, next) => {
+    res.status(404).send({ status: 404, message: 'Not found' });
+  });
+
+  try {
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
