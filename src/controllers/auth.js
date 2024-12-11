81bd5fddf1780598ccfc147d0763fa14b26eea07
Iryna R <deutsche.post15@gmail.com>
2024-12-11 20:49:12 +0100

    add login and register

---
+++ src/controllers/auth.js
@@ -0,0 +1,41 @@
+import { registerUser, loginUser } from '../services/auth.js';
+
+export async function registerController(req, res) {
+  const payload = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  const registeredUser = await registerUser(payload);
+
+  res.send({
+    status: 201,
+    message: 'Successfully registered a user!',
+    data: registeredUser,
+  });
+}
+
+export async function loginController(req, res) {
+  const { email, password } = req.body;
+
+  const session = await loginUser(email, password);
+
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: session.refreshTokenValidUntil,
+  });
+
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: session.refreshTokenValidUntil,
+  });
+
+  res.send({
+    status: 200,
+    message: 'Successfully logged in an user!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
+}
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import contactRoutes from './contacts.js';
+import authRoutes from './auth.js';
 
 const router = express.Router();
 
+router.use('/auth', authRoutes);
 router.use('/contacts', contactRoutes);
 
 export default router;
